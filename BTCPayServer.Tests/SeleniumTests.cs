eb6f39c6a6a800711f153e8942c54c130f4d145a
Kukks <evilkukka@gmail.com>
2023-08-24 10:27:49 +0200

    Introduce archive pull payment permission

--- BTCPayServer.Tests/SeleniumTests.cs
+++ BTCPayServer.Tests/SeleniumTests.cs
@@ -905,7 +905,8 @@ namespace BTCPayServer.Tests
                     Policies.CanModifyStoreSettings,
                     Policies.CanCreateNonApprovedPullPayments,
                     Policies.CanCreatePullPayments,
-                    Policies.CanManagePullPayments
+                    Policies.CanManagePullPayments,
+                    Policies.CanArchivePullPayments,
                 });
             AssertPermissions(pageSource, false,
              new[]
